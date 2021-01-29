42b71b9026bd36062d2334996d20c3db9f260043
welton <tomwb1@gmail.com>
2021-01-29 00:23:30 -0300

    first commit

---
+++ scripts/PlayerPhase1.gd
@@ -0,0 +1,48 @@
+extends KinematicBody2D
+
+enum {
+	IDLE, WALK, HIT, EVOLVE
+}
+
+export var speed = 100
+var status = IDLE
+var last_collision_direction = Vector2()
+
+func _ready():
+	$AnimatedSprite.play("idle")
+	pass
+
+func _physics_process(delta):
+	var direction = Vector2()
+	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	direction = direction.normalized()
+	
+#	caso o player sair e voltar pra parede bate denovo
+	if last_collision_direction.x != 0 && direction.x != 0 && last_collision_direction.x == direction.x:
+		last_collision_direction = Vector2()
+	if last_collision_direction.y != 0 && direction.y != 0 && last_collision_direction.y == direction.y:
+		last_collision_direction = Vector2()
+	
+	walk(direction, delta)
+	animations()
+	
+func walk(direction,delta):
+	if status == WALK || status == IDLE:
+		status = WALK
+		var collision  = move_and_collide(direction * speed * delta)
+		if (direction.x !=0 || direction.y != 0) && collision:
+			if last_collision_direction != collision.normal:
+				last_collision_direction = collision.normal
+				status = HIT
+	
+func animations():
+	if status == WALK:
+		$AnimatedSprite.play("walk")
+	elif status == HIT:
+		$AnimatedSprite.play("hit")
+		var t = get_tree().create_timer(2)
+		yield(t,"timeout")
+		status = IDLE
+	else :
+		$AnimatedSprite.play("idle")
