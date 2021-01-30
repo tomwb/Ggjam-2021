ec3106c64beccebffb8fcdff2ef94301a857117a
welton <tomwb1@gmail.com>
2021-01-30 03:17:43 -0300

    fase 1

--- scripts/PlayerPhase1.gd
+++ scripts/PlayerPhase1.gd
@@ -4,7 +4,10 @@ enum {
 	IDLE, WALK, HIT, EVOLVE
 }
 
-export var speed = 100
+export var max_speed = 300
+export var hit_speed = 50
+var acceleration_time = 0.05
+var acceleration = Vector2()
 var status = IDLE
 var last_collision_direction = Vector2()
 
@@ -23,9 +26,9 @@ func _physics_process(delta):
 		last_collision_direction = Vector2()
 	if last_collision_direction.y != 0 && direction.y != 0 && last_collision_direction.y == direction.y:
 		last_collision_direction = Vector2()
-		
 	
 	walk(direction, delta)
+	hit(delta)
 	animations()
 	
 func walk(direction,delta):
@@ -35,21 +38,36 @@ func walk(direction,delta):
 			status = WALK
 		else:
 			status = IDLE
-		
-		var collision  = move_and_collide(direction * speed * delta)
+			
+		acceleration.x = lerp(acceleration.x, max_speed * direction.x, acceleration_time)
+		acceleration.y = lerp(acceleration.y, max_speed * direction.y, acceleration_time)
+				
+		var collision = move_and_collide(acceleration * delta)
 		if (direction.x !=0 || direction.y != 0) && collision:
-			if last_collision_direction != collision.normal:
-				last_collision_direction = collision.normal
-				status = HIT
-				# daqui a 4 segundos volta pro idle
-				var t = get_tree().create_timer(2.5)
-				yield(t,"timeout")
-				status = IDLE
+			acceleration = Vector2()
+			last_collision_direction = collision.normal
+			$AudioHit.play()
+			status = HIT
+			# quando a animação acabar
+			$AnimatedSprite.play("hit")
+			$AnimationLightMask.play("show")
+			yield($AnimatedSprite,"animation_finished")
+			status = IDLE
+				
+func hit(delta):
+	if status == HIT:
+		move_and_collide(last_collision_direction * hit_speed * delta)
 	
 func animations():
 	if status == WALK:
 		$AnimatedSprite.play("walk")
-	elif status == HIT:
-		$AnimatedSprite.play("hit")
-	else :
+	elif status == IDLE :
 		$AnimatedSprite.play("idle")
+
+
+func _on_CollectorArea2D_area_entered(area):
+	if area.is_in_group("MemoryItem") :
+		area.queue_free()
+		$AnimatedSprite.play("evolve")
+		$AnimationLightMask.play("finish")
+		status = EVOLVE
