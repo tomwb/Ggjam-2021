2d5012046b0fd97e557e6931a4d1ef037df8aa46
Efraim <efraimmarcatto@gmail.com>
2021-01-31 04:44:49 -0300

    Adding ThunderBall and drawing phase 4

---
+++ scripts/ThunderBall.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+export(int, "horizontal", "vertical") var direction = 0
+export var distance_total = 2
+export var distance = 0
+
+func _ready():
+	pass
+	
+
+func _process(delta):
+	pass
+	
